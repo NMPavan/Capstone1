d7593461c3a7f8c0f179f66fdc1a33015def28ff
NMPavan <manikyapavan123@gmail.com>
2019-07-29 13:13:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/archiverfirstpage/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.archiverfirstpage;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private ActionBar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+
+
+}
